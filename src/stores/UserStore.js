ec178e40b35ab200267cbd6df0a1de8a526f494f
Dmitrii Borozdin <borozdin1@yandex.ru>
2023-08-06 20:33:38 +0700

    Добавлены инструменты разработчика для mobx

--- src/stores/UserStore.js
+++ src/stores/UserStore.js
@@ -1,47 +1,48 @@
 import { makeObservable, action, observable, computed } from "mobx";
+import { injectStores } from "@mobx-devtools/tools";
 
 class User {
-    user = null;
+  user = null;
 
-    constructor() {
-        makeObservable(this, {
-            user: observable,
-            fetchUser: action,
-            getUser: computed,
-            createUser: action,
-            deleteUser: action,
-        })
+  constructor() {
+    makeObservable(this, {
+      user: observable,
+      fetchUser: action,
+      getUser: computed,
+      createUser: action,
+      deleteUser: action,
+    });
+  }
+
+  fetchUser = async () => {
+    try {
+      const response = await fetch("URL_TO_GET_USER");
+
+      if (response.ok) {
+        const data = await response.json();
+        this.user = data;
+      } else {
+        throw new Error(response.status);
+      }
+    } catch (error) {
+      console.error("Error fetching user:", error);
     }
+  };
 
-    fetchUser = async () => {
-        try {
-            const response = await fetch('URL_TO_GET_USER');
+  get getUser() {
+    return this.user;
+  }
 
-            if (response.ok) {
-                const data = await response.json();
-                this.user = data;
-            } else {
-                throw new Error(response.status)
-            }
+  createUser(user) {
+    this.user = user;
+  }
 
-        } catch (error) {
-            console.error('Error fetching user:', error);
-        }
-    }
-
-    get getUser() {
-        return this.user;
-    }
-
-    createUser(user) {
-        this.user = user;
-    }
-
-    deleteUser() {
-        this.user = null;
-    }
+  deleteUser() {
+    this.user = null;
+  }
 }
 
 const userStore = new User();
+injectStores({ userStore });
 
 export default userStore;
