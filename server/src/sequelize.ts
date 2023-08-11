7c149f95fb89538f40bac7fe451522bff3d55f79
Sofya Voropay <sofi.tatarinowa@yandex.ru>
2023-08-11 18:33:15 +0300

    application model

--- server/src/sequelize.ts
+++ server/src/sequelize.ts
@@ -2,5 +2,5 @@ import { Sequelize } from "sequelize-typescript";
 
 export const sequelize = new Sequelize(process.env.DB_URI!, {
     dialect: "postgres",
-    models: [`${__dirname}/models/User.ts`]
+    models: [`${__dirname}/models/User.ts`, `${__dirname}/models/Application.ts`]
 })
